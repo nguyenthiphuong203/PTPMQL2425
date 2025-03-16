6e7bdafc91936b7fd8752a95d89a67426fd208a7
nguyenthiphuong203 <nguyenthiphuong080412@gmail.com>
2025-03-16 20:29:21 +0700

    BTH

--- PTPMQL/DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
+++ PTPMQL/DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2596fe6e13e3849d7be59549e6dd126bac5914d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090449f729655a31c46cb2466d539b67cfe1df42")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
